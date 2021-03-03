61831180756ab44f33b048c5043a99575ef12590
Rajat Gundi <glrajat@gmail.com>
2021-03-03 22:21:27 +0530

    Added: ICrud interface for database operations

---
+++ src/main/scala/com/bridgelabz/bookstore/database/interfaces/ICrud.scala
@@ -0,0 +1,36 @@
+package com.bridgelabz.bookstore.database.interfaces
+
+import scala.concurrent.Future
+
+trait ICrud[T] {
+  /**
+   *
+   * @param entity object to be created in the database
+   * @return any status identifier for the create operation
+   */
+  def create(entity: T): Future[Any]
+
+  /**
+   *
+   * @return sequence of objects in the database
+   */
+  def read(): Future[Seq[T]]
+
+  /**
+   *
+   * @param identifier parameter of the (object in the database to be replaced/updated)
+   * @param entity     new object to override the old one
+   * @param fieldName  name of the parameter in the object defined by the identifier
+   * @return any status identifier for the update operation
+   */
+  def update(identifier: Any, entity: T, fieldName: String): Future[Any]
+
+  /**
+   *
+   * @param fieldName  name of the parameter in the object defined by the identifier
+   * @param identifier parameter of the (object in the database to be deleted)
+   * @return any status identifier for the update operation
+   */
+  def delete(identifier: Any, fieldName: String): Future[Any]
+
+}
