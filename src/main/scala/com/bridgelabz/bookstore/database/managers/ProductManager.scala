a0db95840d225ff88707369b6f61edef5e605282
RuchirDixit <ruchirtd96@gmail.com>
2021-03-11 22:09:55 +0530

    [Ruchir]Refactored: Changed from ProductCase to Product model

--- src/main/scala/com/bridgelabz/bookstore/database/managers/ProductManager.scala
+++ src/main/scala/com/bridgelabz/bookstore/database/managers/ProductManager.scala
@@ -2,20 +2,20 @@ package com.bridgelabz.bookstore.database.managers
 
 import com.bridgelabz.bookstore.database.interfaces.ICrud
 import com.bridgelabz.bookstore.exceptions.ProductDoesNotExistException
-import com.bridgelabz.bookstore.models.ProductCase
 import com.typesafe.scalalogging.LazyLogging
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
+import com.bridgelabz.bookstore.models.Product
 
-class ProductManager(productDatabase : ICrud[ProductCase]) extends LazyLogging{
+class ProductManager(productDatabase : ICrud[Product]) extends LazyLogging{
 
   /**
    *
    * @param product : product to be added in database
    * @return : Future of true if added successfully or else future of false
    */
-  def addProduct(product: ProductCase) : Future[Boolean] = {
+  def addProduct(product: Product) : Future[Boolean] = {
     productDatabase.create(product).transform({
       case Success(_) => Success(true)
       case Failure(_) => Success(false)
@@ -27,9 +27,9 @@ class ProductManager(productDatabase : ICrud[ProductCase]) extends LazyLogging{
    * @param fieldName : Field name by which product is to be searched
    * @return : Future of product if found or else product not found exception
    */
-  def getProduct(fieldName : String) : Future[ProductCase] = {
+  def getProduct(fieldName : String) : Future[Product] = {
     var doesExist = false
-    var productFound : ProductCase = null
+    var productFound : Product = null
     productDatabase.read().map(products => {
       products.foreach(product => {
         if(product.author.equals(fieldName) || product.title.equals(fieldName)){
