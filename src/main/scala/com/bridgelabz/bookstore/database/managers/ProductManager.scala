e61544f17e71fa424084d14d4ad77970cadaa411
RuchirDixit <ruchirtd96@gmail.com>
2021-03-12 14:10:47 +0530

    [Ruchir]Refactored: To add userId and userDatabase

--- src/main/scala/com/bridgelabz/bookstore/database/managers/ProductManager.scala
+++ src/main/scala/com/bridgelabz/bookstore/database/managers/ProductManager.scala
@@ -1,25 +1,39 @@
 package com.bridgelabz.bookstore.database.managers
 
 import com.bridgelabz.bookstore.database.interfaces.ICrud
-import com.bridgelabz.bookstore.exceptions.ProductDoesNotExistException
+import com.bridgelabz.bookstore.exceptions.{AccountDoesNotExistException, ProductDoesNotExistException}
 import com.typesafe.scalalogging.LazyLogging
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
-import com.bridgelabz.bookstore.models.Product
+import com.bridgelabz.bookstore.models.{Product, User}
 
-class ProductManager(productDatabase : ICrud[Product]) extends LazyLogging{
+class ProductManager(productDatabase : ICrud[Product], userDatabase : ICrud[User]) extends LazyLogging{
 
   /**
    *
    * @param product : product to be added in database
    * @return : Future of true if added successfully or else future of false
    */
-  def addProduct(product: Product) : Future[Boolean] = {
-    productDatabase.create(product).transform({
-      case Success(_) => Success(true)
-      case Failure(_) => Success(false)
+  def addProduct(userId: String,product: Product) : Future[Boolean] = {
+    var isExist = false
+    userDatabase.read().map(users => {
+      for (user <- users) {
+        if (userId.equals(user.userId)) {
+          isExist = true
+        }
+      }
     })
+    if(isExist){
+      productDatabase.create(product).transform({
+        case Success(_) => Success(true)
+        case Failure(_) => Success(false)
+      })
+    }
+    else {
+      throw new AccountDoesNotExistException
+    }
   }
 
   /**
