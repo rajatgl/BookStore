f243c81f404e65dee8bf58e4a6ae5d42cbf17a27
RuchirDixit <ruchirtd96@gmail.com>
2021-03-12 14:14:47 +0530

    [Ruchir]Refactored: Test cases for Product manager

--- src/test/scala/com/bridgelabz/bookstoretest/managers/ProductManagerTest.scala
+++ src/test/scala/com/bridgelabz/bookstoretest/managers/ProductManagerTest.scala
@@ -5,7 +5,7 @@ import com.bridgelabz.bookstore.database.managers.ProductManager
 import com.bridgelabz.bookstore.exceptions.ProductDoesNotExistException
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatestplus.mockito.MockitoSugar
-import com.bridgelabz.bookstore.models.Product
+import com.bridgelabz.bookstore.models.{Product, User}
 import com.bridgelabz.bookstoretest.TestVariables
 import org.scalatest.matchers.should.Matchers.{a, convertToAnyShouldWrapper}
 
@@ -18,16 +18,17 @@ import scala.concurrent.{Await, Future}
 
 class ProductManagerTest extends AnyFlatSpec with MockitoSugar {
   val iCrudProductMock: ICrud[Product] = mock[ICrud[Product]]
-  val productManager: ProductManager = new ProductManager(iCrudProductMock)
+  val iCrudUserMock: ICrud[User] = mock[ICrud[User]]
+  val productManager: ProductManager = new ProductManager(iCrudProductMock,iCrudUserMock)
 
   "Add Product" should "return true if product added successfully" in {
     when(iCrudProductMock.create(TestVariables.product())).thenReturn(Future(true))
-    assert(Await.result(productManager.addProduct(TestVariables.product()),1500.seconds))
+    assert(Await.result(productManager.addProduct(TestVariables.user().userId,TestVariables.product()),1500.seconds))
   }
 
   "Get product" should "return true if product fetched successfully" in {
     when(iCrudProductMock.read()).thenReturn(Future[Seq[Product]](Seq[Product](TestVariables.product())))
-    assert(Await.result(productManager.getProduct("Xrnes"),1500.seconds) == Product("530","Xrnes","TestProduct","12323434","2",3000.0,"This is a test product"))
+    assert(Await.result(productManager.getProduct("Xrnes"),1500.seconds) == Product("530","Xrnes","TestProduct","12323434","2","3000.0","This is a test product"))
   }
 
   "Get Product which doesn't exist" should "return Product Not found exception" in {
