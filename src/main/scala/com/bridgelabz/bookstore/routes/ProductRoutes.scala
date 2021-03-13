a68001542d3d082fa599b3e3ed712136158c1fe2
RuchirDixit <ruchirtd96@gmail.com>
2021-03-13 14:09:41 +0530

    [Ruchir]Added: Get product route

--- src/main/scala/com/bridgelabz/bookstore/routes/ProductRoutes.scala
+++ src/main/scala/com/bridgelabz/bookstore/routes/ProductRoutes.scala
@@ -6,15 +6,21 @@ import akka.http.javadsl.model.StatusCodes
 import akka.http.scaladsl.server.{Directives, Route}
 import com.bridgelabz.bookstore.database.managers.ProductManager
 import com.bridgelabz.bookstore.marshallers.{AddProductJsonSupport, OutputMessageJsonSupport}
-import akka.http.scaladsl.server.Directives.{complete, entity, headerValueByName, onComplete, parameters, path, post, respondWithHeaders}
+import akka.http.scaladsl.server.Directives.{complete, entity, get, headerValueByName, onComplete, parameters, path, post}
 import com.bridgelabz.bookstore.jwt.TokenManager
 import com.bridgelabz.bookstore.models.{OutputMessage, Product}
 import com.typesafe.scalalogging.LazyLogging
+import akka.http.scaladsl.server.Directives._
+import com.bridgelabz.bookstore.exceptions.ProductDoesNotExistException
 
-import scala.util.Success
+import scala.util.{Failure, Success}
 
 class ProductRoutes(productManager: ProductManager) extends OutputMessageJsonSupport with AddProductJsonSupport with LazyLogging {
 
+  /**
+   * To add products to database
+   * @return : If user authorized and product added returns Ok or else returns UnAuthorized
+   */
   def addProductRoute: Route = post {
     path("addProduct") {
       entity(Directives.as[Product]) { request =>
@@ -43,4 +49,29 @@ class ProductRoutes(productManager: ProductManager) extends OutputMessageJsonSup
       }
     }
   }
+
+  /**
+   * To get Seq of Products using title or author
+   * @return : Product if product is found or else returns product not found message
+   */
+  def getProductRoute : Route = get {
+    path("products") {
+      parameters('name.as[String]) {
+        (name) =>
+            onComplete(productManager.getProduct(name)){
+              case Success(product) =>
+                complete(OutputMessage(StatusCodes.OK.intValue(),product.toString()))
+              case Failure(exception) =>
+                exception match {
+                  case productNotFound : ProductDoesNotExistException =>
+                    complete(productNotFound.status() -> OutputMessage(productNotFound.status(),productNotFound.getMessage))
+                  case _ =>
+                    complete(StatusCodes.INTERNAL_SERVER_ERROR.intValue() -> OutputMessage(StatusCodes.INTERNAL_SERVER_ERROR.intValue(),
+                      "An internal error occurred. Contact the admin."))
+                }
+            }
+      }
+    }
+  }
+
 }
