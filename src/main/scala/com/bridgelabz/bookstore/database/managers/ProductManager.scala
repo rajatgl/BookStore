1fc9069ad64480de2b1fd7b78567e5a2467964fb
RuchirDixit <ruchirtd96@gmail.com>
2021-03-13 14:08:26 +0530

    [Ruchir]Refactored: Return type of get product method to Seq

--- src/main/scala/com/bridgelabz/bookstore/database/managers/ProductManager.scala
+++ src/main/scala/com/bridgelabz/bookstore/database/managers/ProductManager.scala
@@ -3,12 +3,16 @@ package com.bridgelabz.bookstore.database.managers
 import com.bridgelabz.bookstore.database.interfaces.ICrud
 import com.bridgelabz.bookstore.exceptions.{AccountDoesNotExistException, ProductDoesNotExistException}
 import com.typesafe.scalalogging.LazyLogging
-
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
 import com.bridgelabz.bookstore.models.{Product, User}
 
+/**
+ * Created on 3/11/2021.
+ * Class: ProductManager.scala
+ * Author: Ruchir Dixit.
+ */
 class ProductManager(productDatabase : ICrud[Product], userDatabase : ICrud[User]) extends LazyLogging{
 
   /**
@@ -39,20 +43,20 @@ class ProductManager(productDatabase : ICrud[Product], userDatabase : ICrud[User
   /**
    *
    * @param fieldName : Field name by which product is to be searched
-   * @return : Future of product if found or else product not found exception
+   * @return : Future of Sequence of product if found or else product not found exception
    */
-  def getProduct(fieldName : String) : Future[Product] = {
+  def getProduct(fieldName : String) : Future[Seq[Product]] = {
     var doesExist = false
-    var productFound : Product = null
+    var productSeq: Seq[Product] = Seq()
     productDatabase.read().map(products => {
       products.foreach(product => {
         if(product.author.equals(fieldName) || product.title.equals(fieldName)){
           doesExist = true
-          productFound = product
+          productSeq = product.asInstanceOf[Seq[Product]]
         }
       })
       if(doesExist){
-        productFound
+        productSeq
       }
       else {
         throw new ProductDoesNotExistException
