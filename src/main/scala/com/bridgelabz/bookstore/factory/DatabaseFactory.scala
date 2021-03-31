b21b612f7a569681e9efae73264e9ab31cdb30c7
RuchirDixit <ruchirtd96@gmail.com>
2021-03-31 23:39:30 +0530

    [Ruchir]Refactored: DRY principle

--- src/main/scala/com/bridgelabz/bookstore/factory/DatabaseFactory.scala
+++ src/main/scala/com/bridgelabz/bookstore/factory/DatabaseFactory.scala
@@ -7,21 +7,33 @@ import com.bridgelabz.bookstore.database.mysql.tables.upgraded.{ProductTable2, U
 import com.bridgelabz.bookstore.models.{Otp, Product, User}
 
 object DatabaseFactory{
+  val otpCollection: ICrudRepository[Otp] = new DatabaseCollection2[Otp]("userOtp",CodecRepository.OTP)
   def apply(databaseName : String) = {
     databaseName.toUpperCase() match {
-      case "MONGODB_USER" =>    val userCollection: ICrudRepository[User] = new DatabaseCollection2[User]("users",CodecRepository.USER)
-                                val otpCollection: ICrudRepository[Otp] = new DatabaseCollection2[Otp]("userOtp",CodecRepository.OTP)
+      case "MONGODB_USER" =>    val userCollection: ICrudRepository[User] = getUserDatabase("mongodb")
                                 new UserManager2(userCollection, otpCollection)
-      case "MONGODB_PRODUCT" => val userCollection: ICrudRepository[User] = new DatabaseCollection2[User]("users",CodecRepository.USER)
-                                val productCollection: ICrudRepository[Product] = new DatabaseCollection2[Product]("products",CodecRepository.PRODUCT)
+      case "MONGODB_PRODUCT" => val userCollection: ICrudRepository[User] = getUserDatabase("mongodb")
+                                val productCollection: ICrudRepository[Product] = getProductDatabase("mongodb")
                                 new ProductManager2(productCollection,userCollection)
-      case "MYSQL_USER" =>      val userCollection: ICrudRepository[User] = new UserTable2("users")
-                                val otpCollection: ICrudRepository[Otp] = new DatabaseCollection2[Otp]("userOtp",CodecRepository.OTP)
+      case "MYSQL_USER" =>      val userCollection: ICrudRepository[User] = getUserDatabase("mysql")
                                 new UserManager2(userCollection, otpCollection)
-      case "MYSQL_PRODUCT" =>   val userCollection: ICrudRepository[User] = new UserTable2("users")
-                                val productCollection: ICrudRepository[Product] = new ProductTable2("products")
+      case "MYSQL_PRODUCT" =>   val userCollection: ICrudRepository[User] = getUserDatabase("mysql")
+                                val productCollection: ICrudRepository[Product] = getProductDatabase("mysql")
                                 new ProductManager2(productCollection,userCollection)
 
+    }
+  }
+
+  def getProductDatabase(databaseType : String) : ICrudRepository[Product] = {
+    databaseType.toUpperCase() match {
+      case "MONGODB" => new DatabaseCollection2[Product]("products",CodecRepository.PRODUCT)
+      case "MYSQL" => new ProductTable2("products")
+    }
+  }
+  def getUserDatabase(databaseType : String) : ICrudRepository[User] = {
+    databaseType.toUpperCase() match {
+      case "MONGODB" => new DatabaseCollection2[User]("users",CodecRepository.USER)
+      case "MYSQL" => new UserTable2("users")
     }
   }
 }
